e197e65ab3ce9fdb166511bb2981834f29ef1f54
Jesper Heese <77155517+RgbCat0@users.noreply.github.com>
2024-09-25 13:39:12 +0200

    Bad camera follow added

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    public Transform target;
+
+    [SerializeField]
+    private Quaternion desiredRotation;
+    public float smoothSpeed = 14f;
+    private float _smoothSpeed;
 
     private void FixedUpdate()
     {
-        
+        if (!target)
+            return;
+
+        if (Vector2.Distance(transform.position, target.position) > 15)
+            ForceTeleportToTarget();
+
+        _smoothSpeed = smoothSpeed + Vector2.Distance(transform.position, target.position);
+        var smoothedPosition = Vector3.Lerp(
+            transform.position,
+            target.position,
+            _smoothSpeed * Time.deltaTime
+        );
+        smoothedPosition += new Vector3(0, 0, -10);
+
+        transform.position = smoothedPosition;
     }
+
+    public void ForceTeleportToTarget() => transform.position = target.position;
 }
