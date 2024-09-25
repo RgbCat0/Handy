b532fad19865e045b5066d0bb9336187974dff15
DeanRBLagas <deanlagas@gmail.com>
2024-09-25 14:12:15 +0200

    When the player is within range of an object with the tag "Grabable", it becomes a child object of the player and thus moves with it."

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    public string targetTag = "Grabable";
+    public float detectionRange = 1.5f;
+
+    private GameObject grabbedObject = null;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (grabbedObject == null)
+            {
+                AttemptGrab();
+            }
+            else
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+    private void AttemptGrab()
+    {
+        GameObject[] targetObjects = GameObject.FindGameObjectsWithTag(targetTag);
+        foreach (GameObject targetObject in targetObjects)
+        {
+            float distance = Vector3.Distance(player.transform.position, targetObject.transform.position);
+            if (distance <= detectionRange)
+            {
+                Debug.Log("Player grabbed: " + targetObject.name);
+                grabbedObject = targetObject;
+                grabbedObject.transform.SetParent(player.transform);
+                if (grabbedObject.GetComponent<Rigidbody>() != null)
+                {
+                    Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
+                    if (rb != null)
+                    {
+                        rb.isKinematic = true;
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    private void ReleaseObject()
+    {
+        if (grabbedObject != null)
+        {
+            Debug.Log("Player released: " + grabbedObject.name);
+
+            grabbedObject.transform.SetParent(null);
+            if (grabbedObject.GetComponent<Rigidbody>() != null)
+            {
+                Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.isKinematic = false;
+                }
+            }
+            grabbedObject = null;
+        }
+    }
+}
