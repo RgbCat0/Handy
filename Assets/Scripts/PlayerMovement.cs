8ef840293b7b295a456d8a77b36bcf78c1cccdf9
Jesper Heese <77155517+RgbCat0@users.noreply.github.com>
2024-09-27 09:59:00 +0200

    AAAAAAAAAAAAAAA

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -9,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 10f;
     public bool isGrounded = false;
     private Rigidbody2D rb;
+    public List<Collider2D> colliders;
 
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
@@ -35,10 +37,13 @@ public class PlayerMovement : MonoBehaviour
     private bool CanJump()
     {
         // add debug raycast view
-        if (Physics2D.Raycast(transform.position, Vector2.down, 8f, whatIsGround) && isGrounded)
+        if (Physics2D.Raycast(transform.position, Vector2.down, 8f, whatIsGround))
         {
-            Debug.DrawRay(transform.position, Vector2.down, Color.green, 1f);
-            return true;
+            if (colliders.Any(d => d.IsTouchingLayers(LayerMask.NameToLayer("Ground"))))
+            {
+                Debug.DrawRay(transform.position, Vector2.down, Color.green, 1f);
+                return true;
+            }
         }
         Debug.DrawRay(transform.position, Vector2.down, Color.red, 1f);
         return false;
