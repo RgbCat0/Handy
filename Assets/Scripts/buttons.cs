fb57904f7dc7bcc2004353a8b800519118605489
TurboScooter <tfvandorrestein@gmail.com>
2024-09-27 10:14:15 +0200

    start screen

---
+++ Assets/Scripts/buttons.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour
+{
+
+    public void maingame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
